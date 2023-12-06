d538fe35676d208e75312e421c2584cf0363d704
Ilham Temirgaliev <ilham2004t@gmail.com>
2023-12-06 15:31:30 +0600

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { SideBar } from './components/SideBar/SideBar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <SideBar/>
     </>
   )
 }
