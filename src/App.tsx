025741463903168e0e839de399d52964b076e784
Ilham Temirgaliev <ilham2004t@gmail.com>
2023-12-20 12:33:10 +0600

    changef

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-
-import './App.css'
-import { SideBar } from './components/SideBar/SideBar'
 
 function App() {
 
   return (
     <>
-      <SideBar/>
     </>
   )
 }
