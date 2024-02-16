6c5c804e7522784d686e06f72a8c51c8b9f165d7
aishirin <awrnjoomartova@gmail.com>
2024-02-16 15:16:31 +0600

    lala

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,12 @@ import HomePage from "./pages/HomePage/HomePage.tsx";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 import ProtecteRoute from './protectedRoute/protecteRoute';
 
-import { SideBar } from "./components/SideBar/SideBar.tsx";
+// import { SideBar } from "./components/SideBar/SideBar.tsx";
 import {AufPage} from "./pages/SignupPage/SignupPage.tsx";
-import { AboutPage } from "./pages/AboutPage/AboutPage.tsx";
+import MapPage from "./pages/MapPage/MapPage.tsx";
+import Profile from "./pages/ProfilePage/ProfilePage.tsx";
+import SystemPage from "./pages/SystemPage/SystemPage.tsx";
+import SystemModules from "./modules/SystemModules/SystemModules.tsx";
 import Layout from "./components/Layout/Layout.tsx";
 
 
@@ -33,9 +36,26 @@ const router = createBrowserRouter([
             element: <HomePage />,   
           },
           {
-            path:'about',
-            element:<AboutPage/>
+            path: "map",
+            element: <MapPage />,   
+          },
+          {
+            path: "profile",
+            element: <Profile />,   
+          },
+          {
+            path: "systems",
+            element: <SystemPage />,
+          children:[
+            {
+              index:true,
+              element: <SystemModules />,
+            },
+            { path: ":id",
+            element: <SystemPage/>
           }
+          ]
+          },
         ]
       },
       {
@@ -58,4 +78,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+); 
