3b494c8da7a6c11cdabb71336f4ae9d20c99c06b
Islam Temirgaliev <islam2004tr@mail.ru>
2024-03-15 14:16:32 +0600

    dasda

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import * as ReactDOM from 'react-dom';
-
-// import ReactDOM from "react-dom/client";
-// import ReactDOM from "react/jsx-runtime";
+import * as ReactDOM from "react-dom";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.tsx";
-
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
-import ProtecteRoute from './protectedRoute/protecteRoute';
-
-// import { SideBar } from "./components/SideBar/SideBar.tsx";
-import {AufPage} from "./pages/SignupPage/SignupPage.tsx";
+import ProtecteRoute from "./protectedRoute/protecteRoute";
+import { AufPage } from "./pages/SignupPage/SignupPage.tsx";
 import MapPage from "./pages/MapPage/MapPage.tsx";
 import Profile from "./pages/ProfilePage/ProfilePage.tsx";
 import SystemPage from "./pages/SystemPage/SystemPage.tsx";
@@ -23,77 +17,68 @@ import ForgotPage from "./pages/ForgotPage/ForgotPage.tsx";
 import LinkPage from "./pages/LinkPage/LinkPage.tsx";
 import DynamicsRoute from "./pages/DynamicsRoute/DynamicsRoute.tsx";
 
-
-
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
       {
-      
-          element: <ProtecteRoute children={<Layout />} />, 
-       
-         children: [
+        element: <ProtecteRoute children={<Layout />} />,
+
+        children: [
           {
             index: true,
             element: <HomePage />,
           },
           {
             path: "homepage",
-            element: <HomePage />,   
+            element: <HomePage />,
           },
           {
             path: "map",
-            element: <MapPage />,   
+            element: <MapPage />,
           },
           {
             path: "profile",
-            element: <Profile />,   
-          },{
-            path: "/profile/change",
-            element: <ChangePage />,   
+            element: <Profile />,
           },
-          
+          {
+            path: "/profile/change",
+            element: <ChangePage />,
+          },
+
           {
             path: "systems",
             element: <SystemPage />,
-          children:[
-            {
-              index:true,
-              element: <SystemModules />,
-            },
-            { path: ":id",
-            element: <DynamicsRoute
-            />
-          }
-          ]
+            children: [
+              {
+                index: true,
+                element: <SystemModules />,
+              },
+              { path: ":id", element: <DynamicsRoute /> },
+            ],
           },
-        ]
+        ],
       },
       {
-        path:"signin",
-        element: <LoginPage />  
-      }, 
+        path: "signin",
+        element: <LoginPage />,
+      },
       {
-        path:"signap",
-        element: <AufPage />  
-      }, 
+        path: "signap",
+        element: <AufPage />,
+      },
       {
         path: "forgot",
-        element: <ForgotPage />,   
-      },{
+        element: <ForgotPage />,
+      },
+      {
         path: "link",
-        element: <LinkPage />,   
+        element: <LinkPage />,
       },
     ],
   },
-
 ]);
-
-
-
 
 ReactDOM.render(
   <React.StrictMode>
@@ -101,4 +86,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );
-
